fff5a48248ea54b667b4be554131ce43deeb73dd
Sam <sam_holt30@hotmail.co.uk>
2017-10-06 12:21:28 +0100

    Now plots any number ofspecified .tsv files as separate data series

--- 3d_plt.py
+++ 3d_plt.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-import re 
+# Produces a 3D scatter plot from a specified .tsv file
+
+import sys
+import re
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import numpy
 
-fname = 'speed_rpen.tsv'
-entry = '(Speed)'
+try:
+    fnames = sys.argv[1:]
+except:
+    print("\nUsage: 3d_plt.py <input1.tsv> <input2.tsv> ...\n")
 
-#Getting the data in:
-with open(fname, "r") as tsv:
-    lines = tsv.readlines()
-
-
-    
-headPat = entry + '\t(Simple)\t(Read\.Until)\t(RejPen)'
-dataPat = '(\d+)\t(\d+)\t(\d+)\t(\d+)'
+# Getting the data in:
+headPat = '(\w+)\t(Simple)\t(Read\.Until)'
+dataPat = '(\d+)\t(\d+)\t(\d+)'
 
 heads = []
 param = []
@@ -21,23 +22,28 @@ simps = []
 rUnts = []
 inter = []
 
-for i in lines:
-        hmatch = re.match(headPat,i)
+for i in range(0, len(fnames)):
+    with open(fnames[i], "r") as tsv:
+        content = tsv.readlines()
+
+    param.append([])
+    simps.append([])
+    rUnts.append([])
+
+    for line in content:
+        hmatch = re.match(headPat, line)
         if hmatch:
             heads.append(hmatch.group(1))
             heads.append(hmatch.group(2))
             heads.append(hmatch.group(3))
-            heads.append(hmatch.group(4))
-        dmatch = re.match(dataPat,i)
+
+        dmatch = re.match(dataPat, line)
         if dmatch:
-            param.append(int(dmatch.group(1)))
-            simps.append(int(dmatch.group(2)))
-            rUnts.append(int(dmatch.group(3)))
-            inter.append(int(dmatch.group(4)))
+            param[i].append(int(dmatch.group(1)))
+            simps[i].append(int(dmatch.group(2)))
+            rUnts[i].append(int(dmatch.group(3)))
 
-
-#Graphing the data
-
+# Graphing the data
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -45,10 +51,14 @@ xpon = rUnts
 ypon = simps
 zpon = param
 
-ax.scatter(xpon, ypon, zpon)
-ax.set_xlabel('Read until (h)')
-ax.set_ylabel('Simple (h)')
-ax.set_zlabel('Speed')
+for i in range(0, len(fnames)):
+    ax.scatter(xpon[i], ypon[i], zpon[i],
+               color=numpy.random.rand(3,),
+               label=fnames[i])
+    ax.legend()
+
+ax.set_xlabel(heads[2])
+ax.set_ylabel(heads[1])
+ax.set_zlabel(heads[0])
 
 plt.show()
-
