64602aabd34419b1e825cc4c4e93b30122b60a9e
Sam <sam_holt30@hotmail.co.uk>
2017-10-10 14:56:48 +0100

    npore.py: minor readability updates

--- npore.py
+++ npore.py
@@ -13,19 +13,20 @@ class Npore:
         self.reads = []
 
     ## Experiment Methods
-    def Simple(self, libList):
+    def simple(self, libList):
         sel = Select(libList)
         # Should call Select() on libList; return lib
 
         # Then call Pore() on selected lib
 
         # Add duration to read and to self.duration
+        # Duration inc. seqTime, interval, any rejPen
         # Need to call add_coverage() Library method
 
-    def ReadUntil(self, libList):
+    def readUntil(self, libList):
         # Similar to above, but with read until characteristics
 
-    def Select(self, libList):
+    def select(self, libList):  # Copy-paste from run.py
         bag = 0
         for obj in libList:
             entries = obj.gsize * obj.ratio
@@ -44,22 +45,28 @@ class Npore:
             else:
                 i += 1
 
-    def Pore(self, selection):
-        
+    def pore(self, selection):
+        # In run.py as it stands, this is where get_read() is called
+        # Perform length calc and time calc; add to Library
 
     ## Read Record Methods
-    def ReadGen(self, lib)
+    def readGen(self, lib)
         
         self.reads.append([])
 
         newRead = lib.get_read()
 
-        self.reads[-1].append(lib)              # libref
-        self.reads[-1].append(newRead[0])       # read start base
-        self.reads[-1].append(newRead[1])       # read end base
-        self.reads[-1].append(self.duration)    # read start time
+        self.reads[-1].append(lib)              # [0] libref
+        self.reads[-1].append(newRead[0])       # [1] read start base
+        self.reads[-1].append(newRead[1])       # [2] read end base
+        self.reads[-1].append(self.duration)    # [3] read start time
 
-    def ReadRec(self, time):
+        return self.reads[-1]   # Is this a good idea?
+
+    def readRec(self, time):
         # This method should record the duration required by a given read
-        # Record its interval and rejPen (where applicable)
-        self.reads[-1].append(time)               # read duration
+        # Include its interval and rejPen (where applicable)
+        self.reads[-1].append(time)               # [4] read duration
+
+    def prevReadStart(self):
+        return self.reads[-1][3]
